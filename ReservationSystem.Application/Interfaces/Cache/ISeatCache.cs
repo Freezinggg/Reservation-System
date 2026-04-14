db15d6fefd1db8dc31f618905c36c465eb75cc53
Freezinggg <blerubs@gmail.com>
2026-04-14 14:50:27 +0700

    add cache for remainingseat

---
+++ ReservationSystem.Application/Interfaces/Cache/ISeatCache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Application.Interfaces.Cache
+{
+    public interface ISeatCache
+    {
+        Task<int?> GetRemainingAsync(Guid seatCategoryId);
+        Task DecrementAsync(Guid seatCategoryId, int quantity);
+    }
+}
---
+++ ReservationSystem.Infrastructure/Connection/RedisSeatCache .cs
@@ -0,0 +1,37 @@
+﻿using ReservationSystem.Application.Interfaces.Cache;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Infrastructure.Connection
+{
+    public class RedisSeatCache : ISeatCache
+    {
+        private readonly IDatabase _db;
+
+        public RedisSeatCache(IConnectionMultiplexer redis)
+        {
+            _db = redis.GetDatabase();
+        }
+
+        public async Task DecrementAsync(Guid seatCategoryId, int quantity)
+        {
+            var key = $"seat:{seatCategoryId}:remaining";
+
+            await _db.StringDecrementAsync(key, quantity);
+            await _db.KeyExpireAsync(key, TimeSpan.FromSeconds(20));
+        }
+
+        public async Task<int?> GetRemainingAsync(Guid seatCategoryId)
+        {
+            var key = $"seat:{seatCategoryId}:remaining";
+
+            var value = await _db.StringGetAsync(key);
+
+            return value.HasValue ? (int)value : null;
+        }
+    }
+}
